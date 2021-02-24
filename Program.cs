0db94ccaaa55fd9681b7892bfedf200f46f37acc
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2021-02-24 13:33:56 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD! I AM READY! >;) ");
+        }
+    }
+}
