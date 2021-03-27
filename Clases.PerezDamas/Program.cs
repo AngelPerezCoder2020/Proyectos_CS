446588bfce5a1ac2c79346becdda8d26d1ee0d37
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2021-03-27 11:10:36 -0600

    Creacion del Programa de manejo de clases en CS

---
+++ Clases.PerezDamas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Clases.PerezDamas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            int edad;
+            String carnet;
+            Boolean solve;
+            Materia clase;
+            int c = 0;
+            Estudiante[] lista = new Estudiante[5];
+            while(c<5){
+                Console.WriteLine("Ingrese Nombre Del Alumno");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Clases.PerezDamas/obj/estudiantes.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Clases.PerezDamas{
+    class Estudiante{
+        public String nombre;
+        public int edad;
+        public String carnet;
+        public Boolean solve;
+        public materia materia;
+        public estudiante(){
+
+        }
+    }
+}
